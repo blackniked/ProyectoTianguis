3827fddaf2d9cf6bd3aa23c00ea6e60c6dd440d9
Jesus <jose.martin4431@alumnos.udg.mx>
2024-06-30 14:59:05 -0600

    Sidebar y navbar creados

--- proyecto-tianguis/src/app/app.component.ts
+++ proyecto-tianguis/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './tianguis-app/pages/home/home-page/home-page.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent, SidebarComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
