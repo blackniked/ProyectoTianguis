9bd44cf433058bd77e30b443a34b5c9795348c68
Jesus <jose.martin4431@alumnos.udg.mx>
2025-05-16 18:27:42 -0600

    Deploy para bearer token

--- src/app/core/services/registroprods.service.ts
+++ src/app/core/services/registroprods.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RegistroProdsService {
-  private apiUrl = 'https://18.116.15.141';
+  private apiUrl = 'https://ez-market.shop/productos';
 
   constructor(private http: HttpClient) {}
 
   registrarProducto(producto: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/productos/`, producto);
-  }
+  const token = localStorage.getItem('token');
+  const headers = new HttpHeaders({
+    'Authorization': `Bearer ${token}`
+  });
+  return this.http.post(`${this.apiUrl}/`, producto, { headers });
+}
 }
